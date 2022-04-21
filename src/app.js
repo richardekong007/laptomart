0f05979d45ada19c0258f7a5f77f55479a159ce9
Richard <richardekong290@gmail.com>
2022-04-21 23:06:06 +0100

    import, reference and use the product component in this file

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 import Home from "../src/component/Home.js";
 import About from "../src/component/About.js";
 import Review from "../src/component/Review.js";
+import Product from "../src/component/Product.js";
 
 export default class App {
 
@@ -8,12 +9,14 @@ export default class App {
     #home;
     #about;
     #review;
+    #product;
 
     constructor(container) {
         this.#container = container;
         this.#home = new Home(this.#container);
         this.#about = new About(this.#container);
         this.#review = new Review(this.#container);
+        this.#product = new Product(this.#container);
     }
 
     init = () => {
