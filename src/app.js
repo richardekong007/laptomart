3dc6380ceb5d1c8466165d1118698ace2ab8a7dc
Richard <richardekong290@gmail.com>
2022-04-21 22:29:10 +0100

    refactor this file

--- src/app.js
+++ src/app.js
@@ -1,24 +1,27 @@
 import Home from "../src/component/Home.js";
+import About from "../src/component/About.js";
 
 export default class App {
 
     #container;
     #home;
+    #about;
 
     constructor(container) {
         this.#container = container;
         this.#home = new Home(this.#container);
+        this.#about = new About(this.#container);
     }
 
     init = () => {
-        this.render()
-    }
+        this.render();
+    };
 
     render = () => {
-        this.#home.render()
-    }
+        this.#home.render();
+    };
 
     addEventListeners = () => {
-    }
+    };
 }
 
