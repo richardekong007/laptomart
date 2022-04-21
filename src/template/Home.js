b593963f61ecacdb0e1472b344628421c1b3e5ed
Richard <richardekong290@gmail.com>
2022-04-21 21:20:50 +0100

    refactor method declaration style to es6 arrow function

---
+++ src/template/Home.js
@@ -0,0 +1,131 @@
+export const render = () => {
+    return `
+ <div id="container">
+    <div id="section1">
+            <p id="headline">
+                Experience the efficiency behind moderately used mid-ranged, clean and affordable laptops
+            </p>
+            <br>
+            <span id="search-label">Find your best deal</span>
+            <div id="search-bar">
+                <input type="text" placeholder="Search by manufacturer, model or specifications"/>
+                <img id="search-btn" src="/public/icon/search-icon.svg" alt="search-icon"/>
+            </div>
+    </div>
+    <div id="section2" class="section">
+	    <div class="section-header">
+		    <img src="/public/icon/group.svg" alt="group-icon">
+			    <p>We strive to fulfill your desires by connecting you to 
+                   moderately used laptops which are
+			    </p>	
+	    </div>
+	    <div class="section-list">
+		    <ul>
+			    <li>Affordable</li>
+			    <li>Reliable</li>
+			    <li>Durable</li>
+                <li>Ergonomic</li>
+                <li>Customizable</li>
+                <li>Resalable</li>
+                <li>Maintainable</li>
+                <li>Upgradable</li>
+		    </ul>
+	    </div>
+    </div>
+    <div class="section">
+	        <div class="section-header">
+		        <img src="/public/icon/gear.svg" alt="gear">
+		        <p>We Are Your Best Bet</p>
+	        </div>
+	        <div id="section3-contents">
+                <div class="sub-section">
+                    <div class="sub-section-header">
+                        <img src="/public/icon/refund.svg" alt="refund">
+                        <p>7 Days refund/return-back policy</p>
+                    </div>
+                    <div class="desc">
+                        We accept all returns made on devices
+                        which have been purchased within 7 days, especially
+                        as a result of certain unintentional defects incurred
+                        by our team or certain defects not discovered before 
+                        purchase.   
+                    </div>
+                </div>
+                <div class="sub-section">
+                    <div class="sub-section-header">
+                        <img src="/public/icon/van.svg" alt="van">
+                        <p>Delivery Service</p>
+                    </div>
+                    <div class="desc">
+                        We work with our trusted experts to deliver your devices safely
+                        to any convenient city within the nation, especially after 
+                        processing your payments.
+                    </div>
+                </div>
+                <div class="sub-section">
+                    <div class="sub-section-header">
+                        <img src="/public/icon/maintenance.svg" alt="maintenance">
+                        <p>Maintenance</p>
+                    </div>
+                    <div class="desc">
+                        You are assured of being connected to our trusted
+                        system engineers, even 7 days after purchasing the 
+                        product
+                    </div>
+                </div>
+                <div class="sub-section">
+                    <div class="sub-section-header">
+                        <img src="/public/icon/customize.svg" alt="customize">
+                        <p>Device Customization</p>
+                    </div>
+                    <div class="desc">
+                        Tell us what you need and let's work towards satisfying 
+                        you by fine-tuning your device in accordance with your request.
+                    </div>
+                </div>
+            </div>
+    </div>
+    <div id="section4" class="section">
+        <div class="section-header">
+            <img src="/public/icon/benefit.svg" alt="benefit">
+            <p>What you get for your money?</p>
+        </div>
+        <div class="section-list">    
+            <ul>
+                <li>Moderately used laptops.</li>
+                <li>High-end device with the best of memory and storage specifications</li>
+                <li>Durable power adapters.</li>
+                <li>Activated operating system equipped with office productivity tools</li>
+            </ul>
+        </div>
+    </div>
+    <div class="section">
+        <div class="section-header">
+            <img src="/public/icon/feature.svg" alt="feature">
+            <p>Features</p>
+        </div>
+        <div id="feature-section">    
+            <div>
+                <img src="/public/icon/power.svg" alt="power">
+                <b>Durable Chargers</b>
+            </div>
+            <div>
+                <img src="/public/icon/battery.svg" alt="battery">
+                <b>Battery of energy capacity between normal and excellent</b>
+            </div>
+            <div>
+                <img src="/public/icon/memory.svg" alt="memory">
+                <b>Memories of various capacities</b>
+            </div>
+            <div>
+                <img src="/public/icon/disk.svg" alt="disk">
+                <b>Functional Operating System and productivity software</b>
+            </div>
+        </div>   
+    </div>
+    <div class="section-header section">
+        <img src="/public/icon/terms.svg" alt="info">
+        <p>Terms and conditions applied</p>
+    </div> 
+</div>`;
+};
