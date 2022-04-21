6c2c2fd14f119f1c532bf5f36f9459af05fe5c22
Richard <richardekong290@gmail.com>
2022-04-21 22:57:40 +0100

    import and use the Review component

--- src/app.js
+++ src/app.js
@@ -1,16 +1,19 @@
 import Home from "../src/component/Home.js";
 import About from "../src/component/About.js";
+import Review from "../src/component/Review.js";
 
 export default class App {
 
     #container;
     #home;
     #about;
+    #review;
 
     constructor(container) {
         this.#container = container;
         this.#home = new Home(this.#container);
         this.#about = new About(this.#container);
+        this.#review = new Review(this.#container);
     }
 
     init = () => {
