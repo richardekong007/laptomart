e9dec1933e47a5675360e07a6908701eb0a7245c
Richard <richardekong290@gmail.com>
2022-04-22 21:45:39 +0100

    save transpile es6 output to this file

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ import About from "../src/component/About.js";
 import Review from "../src/component/Review.js";
 import Product from "../src/component/Product.js";
 import Contact from "../src/component/Contact.js";
+import Menu from "../src/component/Menu.js";
 
 export default class App {
 
@@ -12,6 +13,7 @@ export default class App {
     #review;
     #product;
     #contact;
+    #menu;
 
     constructor(container) {
         this.#container = container;
@@ -20,17 +22,44 @@ export default class App {
         this.#review = new Review(this.#container);
         this.#product = new Product(this.#container);
         this.#contact = new Contact(this.#container);
+        this.#menu = new Menu();
     }
 
     init = () => {
         this.render();
+        this.#addEventListeners();
     };
 
     render = () => {
         this.#home.render();
     };
 
-    addEventListeners = () => {
+    #addEventListeners = () => {
+        this.#homeSectionInflateEvent();
+        this.#productSectionInflateEvent();
+        this.#contactSectionInflateEvent();
+        this.#reviewSectionInflateEvent();
+        this.#aboutSectionInflateEvent();
+    };
+
+    #homeSectionInflateEvent = () =>{
+        this.#home.on(Menu.INFLATE_HOME_SECTION, () => this.#home.render());
+    };
+
+    #productSectionInflateEvent = ()=>{
+        this.#product.on(Menu.INFLATE_PRODUCT_SECTION, () => this.#product.render());
+    };
+
+    #contactSectionInflateEvent = () =>{
+        this.#contact.on(Menu.INFLATE_CONTACT_SECTION, () => this.#contact.render());
+    };
+
+    #reviewSectionInflateEvent = () =>{
+        this.#review.on(Menu.INFLATE_REVIEW_SECTION, () => this.#review.render());
+    };
+
+    #aboutSectionInflateEvent = () =>{
+        this.#about.on(Menu.INFLATE_ABOUT_SECTION, () => this.#about.render());
     };
 }
 
